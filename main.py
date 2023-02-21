6d3a0d5acf180f6de68ab1d69ff0810e2d24018c
anal-ugnetatel <hanter045@yandex.ru>
2023-02-22 00:14:07 +0300

    Приветственный Экран

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
+import telebot
+from telebot import types
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+bot = telebot.TeleBot('6110641577:AAFLP9EcnCD2hH5DS6xTwL8eElNhz3vZeZM')
+
+@bot.message_handler(commands = ['start'])
+def url(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("👋 Поздороваться")
+    btn2 = types.KeyboardButton('🦾 Добавить результат')
+    markup.add(btn1, btn2)
+    bot.send_message(message.from_user.id, " 👋 Поздороваться / \n🦾 Добавить результат", reply_markup=markup)
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == '👋 Поздороваться':
+        img = open('D:\Tg\Homo.PNG', 'rb')
+        bot.send_photo(message.from_user.id, img) #ответ бота
+        bot.send_message(message.from_user.id, "Привет, Гигус младший, здесь начнется твое становление Сигмой. Okeeey, leets gooo!")
+bot.polling(none_stop=True)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
